59b2c36ee1be09076707af1fd13b47e376f90304
Joao Paulo Furtado <joaopaulofurtado@live.com>
2021-05-05 14:39:44 -0700

    turn off

--- src/helpers/TelegramBot.ts
+++ src/helpers/TelegramBot.ts
@@ -1,12 +1,11 @@
 import TelegramBot from "node-telegram-bot-api";
 
 import { telegramBot } from "../app";
-import { telegramBotGroupID } from "../constants/TelegramConstants";
 
 export class TelegramBotHelper {
   public bot: TelegramBot = telegramBot;
 
   public sendMessageToGroup(message: string) {
-    this.bot.sendMessage(telegramBotGroupID, message);
+    // this.bot.sendMessage(telegramBotGroupID, message);
   }
 }
